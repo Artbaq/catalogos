84be14ccfdf6974c93ab517705d6c6a94954e3df
Artbaq <52413566+Artbaq@users.noreply.github.com>
2019-10-08 21:13:57 -0500

    Delete variables.js

--- variables.js
+++
@@ -1,7 +0,0 @@
-var aboutUs = "Articulos Bquilla el ditribuidor de Gorras y Accessorios Tecnologícos <b><em>N°1</em></b> de Barranquilla." +
-			  " Con la mejor calidad y los mejores precios. <br> Permítenos ser tu proveedor de confianza ";
-document.getElementById('aboutUs').innerHTML = aboutUs;
-
-
-
-
