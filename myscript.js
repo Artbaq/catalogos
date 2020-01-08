5eb605f83c2000624ff90889ea51eeeaf1967c29
Artbaq <52413566+Artbaq@users.noreply.github.com>
2020-01-08 01:38:14 -0500

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -5,19 +5,35 @@
 
 var facebook ="fb://facewebmodal/f?href=https://www.facebook.com/articulosbquilla";
 var instagram="https://www.instagram.com/articulos_bquilla";
-var whatsapp = "https://api.whatsapp.com/send?phone=+573008546921";
-var face_art ='fb://page/378010675934018';
-var face_gor ='fb://page/581398665578960';
+var	whatsapp = "https://api.whatsapp.com/send?phone=+573008546921";
+var	face_art ='fb://page/378010675934018';
+var	face_gor ='fb://page/581398665578960';
 var server = "https://artbaq.github.io/catalogo/";
+
+
+const redesSociales = [
+	{ brand: "whatsapp" , icon: "fab fa-whatsapp" , link: "https://api.whatsapp.com/send?phone=+573008546921" },
+	{ brand: "facebook" , icon: "fab fa-facebook" , link: "fb://facewebmodal/f?href=https://www.facebook.com/articulosbquilla" },
+	{ brand: "instagram", icon: "fab fa-instagram" , link: "https://www.instagram.com/articulos_bquilla" },
+	{ brand: "whatsapp" , icon: "fab fa-facebook" , link: "https://api.whatsapp.com/send?phone=+573008546921" }
+];
+
+
+
+
 
 var flechas = '<a class="transparent" style="left:0;" onclick="plusDivs(-1)" >&#10094 </a>'+
  			'<a class="transparent" style="right:0;" onclick="plusDivs(1)">&#10095</a>';
 
-var promo_0 = "promo_0.png Smart TV BOX plus ⛱ Aprovecha que solo quedan pocos.";
-var promo_1 = "promo_1.png Receptor Bluetooth ⛱ Convierte tu viejo equipo de sonido a Bluetooth";
-var promo_2 = "promo_2.png Speaker Bluetooth ⛱ Lleva tu musica a donde quieras.";
-var promo_3 = "promo_3.png Gorras Planas ⛱ Tu mejor estilo para cada ocacion.";
-var promo_4 = "promo_4.png Game pad ⛱ Llevas tus juegos a otro nivel ";
+
+
+const promotion = [
+	{image: "promo_0.png" , title: "Smart TV BOX plus" , senten: "Aprovecha que solo quedan pocos."},
+	{image: "promo_1.png" , title: "Receptor Bluetooth", senten: "Convierte tu viejo equipo de sonido a Bluetooth."},
+	{image: "promo_2.png" , title: "Speaker Bluetooth" , senten: "Lleva tu musica a donde quieras."},
+	{image: "promo_3.png" , title: "Gorras Planas"	   , senten: "Tu mejor estilo para cada ocacion."},
+	{image: "promo_4.png" , title: "Game pad" 		   , senten: "Llevas tus juegos a otro nivel."}
+]
 
 
 //**********************
@@ -25,19 +41,19 @@ var containerModal = document.getElementById('container-modal');
 var mySidebar = document.getElementById("mySidebar");
 var social = document.getElementById('social');
 var modal = document.getElementById('myModal');
-//var menu = document.getElementById('bottom_header');
-//var myNav = document.getElementById("myNav");
-//getMenu(menu); // Hamburger menu - Three dots
-getPromo(promo_0,0);
-getPromo(promo_1,4);
-getPromo(promo_2,2);
-getPromo(promo_3,3);
-getPromo(promo_4,1); 
 
-var txtx = [" Inicio", " Nosotros", " Catalogos", " Contacto"];
-var faLogo = ["fa fa-home", "fa fa-user", "fa fa-list", "fab fa-whatsapp"];
 
-function navLinks(id , idName) {			
+getPromo2();
+
+var txtx = [" Nosotros", " Catalogos", " Contacto"];
+var faLogo = ["fa fa-user", "fa fa-list", "fab fa-whatsapp"];
+
+
+
+
+
+function navLinks(id , idName) {	
+
 	let myDiv = document.createElement('div'); 
 	let myH = document.createElement('h4'); 
 	id.appendChild(myDiv);
@@ -50,6 +66,7 @@ function navLinks(id , idName) {
 		let myA = document.createElement('a');
 		let myI = document.createElement('i'); 
 			
+			myA.className =  
 			myDiv.appendChild(myA);
 			myA.appendChild(myI);
 			myA.appendChild(mySpan);
@@ -64,17 +81,24 @@ function navLinks(id , idName) {
 }
 
 navLinks(bottom_header); 
-bottom_header.getElementsByTagName('a')[3].href= whatsapp;
-//navLinks(myNav); 
+bottom_header.getElementsByTagName("a")[2].href = whatsapp;
+bottom_header.getElementsByTagName("a")[2].style.ccsText = "font-size:26px; font-weight:800";	
+
+
+
 
 var links = [	"oModal('ear_',3)","oModal('cha_',4)","oModal('spe_',2)","oModal('tec_',20)" ];
 var txt = [	"Audifonos & Bluetooth","Cargadores & Cables","Speakers","Miselaneos & más" ];
 var icon = [ "🎧 ", "🔌 " , "📻 ", "► " ];
 
+const category = [{},{},{},{}];
+
 function getCategory(id , idName) {	
+		
 	let myDiv = document.createElement('div'); 
 	let myH = document.createElement('h4');
-	id.appendChild(myDiv);	myDiv.appendChild(myH);
+	id.appendChild(myDiv);
+	myDiv.appendChild(myH)
 	myDiv.id= idName;
 
 	for (var i = 0; i < txt.length; i++) {
@@ -96,17 +120,21 @@ function getCategory(id , idName) {
 			myP2.innerHTML = txt[i];
 
 		if (icon.length == 0)
-		{	myP.appendChild(myImg); 
+		{
+			myP.appendChild(myImg); 
 			myImg.src = imagenes[i];
-		}else{	myP.innerHTML = icon[i];	}
+		}else{	
+			myP.innerHTML = icon[i];				
+		}
 	}
-}
+}// ►►►►►► Calling the function ◄◄◄◄◄◄◄ 
 
 
 var socialLink = ["fb://facewebmodal/f?href=https://www.facebook.com/articulosbquilla","https://www.instagram.com/articulos_bquilla","#","#"]
 var socialFaLogo = [ "fab fa-facebook-square", "fab fa-instagram", "fab fa-linkedin", "fab fa-google-plus-square"];
 
-function socialLinks(id , idName) {		
+function socialLinks(id , idName) {	
+		
 	let myDiv = document.createElement('div'); 
 	let myH = document.createElement('h4'); 
 	id.appendChild(myDiv);
@@ -119,8 +147,9 @@ function socialLinks(id , idName) {
 		let myI = document.createElement('i'); 
 			myDiv.appendChild(myA);
 			myA.appendChild(myI);
-		
-		if (myDiv.id == "undefined") {	myH.style.display ='none';	}
+
+			if (myDiv.id == "undefined") {	myH.style.display ='none';	}
+
 			myH.innerHTML = myDiv.id.substring(2);
 			myI.className = socialFaLogo[i];
 			myA.href = socialLink[i];
@@ -128,36 +157,44 @@ function socialLinks(id , idName) {
 			//myA.innerHTML += txt[i];
 	}
 }
-
-getCategory(techno);
-
 socialLinks(footer , "ftSiguenos");
 
-navLinks(footer , 'ftMenu');
-ftMenu.getElementsByTagName('a')[3].href= whatsapp;
+navLinks(footer , 'ftMenu');  
+ftMenu.getElementsByTagName("a")[2].href = whatsapp;
 
+getCategory(techno);
 getCategory(footer , 'ftTecnología');
 
-   links =  ["oModal('cur_',2)" , "oModal('pla_',6)"];
-	 txt =	["Gorras Cuvadas" , "Gorras Planas"];
+	links = ["oModal('cur_',2)" , "oModal('pla_',6)"];
+	txt =	["Gorras Cuvadas" , "Gorras Planas"];
 imagenes = 	["cur_2.png" , "pla_5.png"]
 	icon = 	[];
 
 getCategory(gorras);	
 getCategory(footer , 'ftGorras');
-//document.getElementById("contactanos").href=contact;
 
-function getPromo(vary,n){
-	var a = vary.search("promo"),
-		b = vary.search("png"),
-	 	c = vary.search("⛱"),
-	 source = vary.substring(a , b + 3),
-	 tittle = vary.substring(b + 3 , c),
-	 coment = vary.substring(c + 1);
 
-	document.getElementsByClassName("promo")[n].src= source;
-	document.getElementsByClassName("enunc")[n].innerHTML= tittle;
-	document.getElementsByClassName("stat")[n].innerHTML= coment;
+function getPromo2(){
+	promotion.forEach(function(item){
+
+		let myDiv = document.createElement("div"); 	myDiv.className = "mySlides fade txt";
+		let myImg = document.createElement("img");	myImg.className = "promo";
+		let myDiv2 = document.createElement("div");	myDiv2.className = "promoTxt";
+		let myH3 = document.createElement("h3");	myH3.className = "enunc";
+		let myp = document.createElement("p");		myp.className = "stat";
+		
+		let slideShow = document.getElementById('slideshow');
+
+				slideShow.appendChild(myDiv)
+				myDiv.appendChild(myImg);
+				myDiv.appendChild(myDiv2);
+				myDiv.appendChild(myH3);
+				myDiv.appendChild(myp);
+
+				myImg.src = item.image;
+				myH3.innerHTML = item.title;
+				myp.innerHTML = item.senten;
+				});
 }
 
 function get_images(y, n)
@@ -177,33 +214,40 @@ function get_images(y, n)
 // Get the modal
 function oModal(nam , num){
 	exit_Icon(myModal);
-    modal.style.display = "block";    
+		modal.style.display = "block";    
     get_images(nam , num)
     showDivs(slideIndex); 
-}
+	}
 
 function exit_Icon(id){
 	var x = id.getElementsByTagName("a")[0]; 
-	x.innerHTML = "&#10006";			
-	x.src = "javascript:void(0)";
-	x.onclick = function(){ w3_close() };
-}
+		x.innerHTML = "&#10006";			
+		x.src = "javascript:void(0)";
+		x.onclick = function(){ w3_close() };
+		}
+
 
 function w3_open(){ 
+	
 	mySidebar.style.backgroundColor = 'rgba(0,0,0,0.8)'; 
 	mySidebar.style.color = 'white'; 
 	mySidebar.style.width = '40%';
 	mySidebar.style.zIndex = '1';
+	//closeClass[0].style.cssText = 'display:block;';
 }
-
 function w3_close()	{
 	modal.style.display = 'none';
-	//this.style.width = '0%';
-	//this.style.color = 'rgba(0,0,0,0.1)'; //'rgba(255,255,255,0.8)'
+	mySidebar.style.width = '0%';
+	mySidebar.style.color = 'rgba(0,0,0,0.1)'; //'rgba(255,255,255,0.8)'
+	
 }
+		
+
+
+var closeClass = document.getElementsByClassName("close");
+
 
 var slideIndex = 1;
-
 function plusDivs(n)	{ 
 	showDivs(slideIndex += n);
 }
@@ -228,21 +272,27 @@ function showDivs(n)	{     var i;
 var slideInd = 1;
 showSlides(slideInd);
 
-function showSlides() {	var i, slides, dots;
-  slides= document.getElementsByClassName("mySlides");
-  dots= document.getElementsByClassName("dot");
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
   
-	for (i=0; i<slides.length; i++) {
-		slides[i].style.display = "none"; 
-  	}
-	slideInd++;
-	if(slideInd>slides.length) {
-  		slideInd = 1
-	}  
-	for (i=0; i<dots.length; i++) {
-    	dots[i].className =
-   		dots[i].className.replace("active","");
-	}
+  
+  //Hide all the slide with a loop
+
+   for (i = 0; i < slides.length; i++) {
+
+    slides[i].style.display = "none"; 
+  }
+
+  slideInd++;
+
+  if (slideInd > slides.length) {slideInd = 1}  
+
+  for (i = 0; i < dots.length; i++) {
+
+    dots[i].className = dots[i].className.replace("active", "");
+  }
 
   slides[slideInd-1].style.display = "block";  
   dots[slideInd-1].className += " active";
@@ -265,23 +315,52 @@ function onBackKeyDown()
   mainView.router.back(); 
 }
 
-var Share = {
-facebook: function(purl, ptitle, pimg, text) {
-url = 'fb://facewebmodal/f?href=https://www.facebook.com/sharer.php?s=100';
-url += '&p[title]=' + encodeURIComponent(ptitle);
-url += '&p[summary]=' + encodeURIComponent(text);
-url += '&p[url]=' + encodeURIComponent(purl);
-url += '&p[images][0]=' + encodeURIComponent(pimg);
-Share.popup(url);
-},
-twitter: function(purl, ptitle) {
-url = 'http://twitter.com/share?';
-url += 'text=' + encodeURIComponent(ptitle);
-url += '&url=' + encodeURIComponent(purl);
-url += '&counturl=' + encodeURIComponent(purl);
-Share.popup(url);
-},
-popup: function(url) {
-window.open(url,'','toolbar=0,status=0,width=626, height=436');
+
+function backTop(){
+	let a = document.createElement('a'); 
+	let i = document.createElement('i');
+			a.id = 'homeButtom';
+			a.href = '#inicio';			
+			i.className = 'fa fa-home';			
+		a.appendChild(i);
+		document.body.appendChild(a);		
+} 
+backTop();	// This is the calling of backTop() function
+
+
+function backTo(){
+	let a = document.createElement('a'); 
+	let i = document.createElement('mark');
+			a.id = 'homeButtom';
+			a.href = '#inicio';
+			i.className = 'fa fa-home';
+			i.innerHTML = '154';	
+		a.appendChild(i);
+		document.body.appendChild(a);		
+} 
+
+
+var homeButton = document.getElementById('homeButtom');
+var emptySpace = document.getElementById('emptySpace');
+
+function scrolling(){	
+	if(bottom_header.offsetTop < window.pageYOffset){
+		bottom_header.classList.add("sticky");	
+		logo.style.opacity='1';	
+		logo.style.transition ='1s';
+		emptySpace.style.cssText ='height:3.4em; background:floralwhite';
+	}else{
+		bottom_header.classList.remove("sticky");
+		logo.style.opacity ='0';
+		logo.style.transition ='0s';
+		emptySpace.style.cssText ='height:0em;';
+	}
 }
-};
+
+
+function backTopTrigger(){
+	if(dotsBox.offsetTop < window.pageYOffset)
+	{		homeButton.style.width="40px";
+		}else{		homeButton.style.width="0px";
+	}			
+}
